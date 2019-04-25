8ed424ea09d90a687342217df32c71cfc49ae1f0
Mitch Samuels <samuels.mitch@gmail.com>
2019-04-25 11:57:47 -0500

    Corrected repos file

--- app/src/js/projects.js
+++ app/src/js/projects.js
@@ -67,7 +67,7 @@ var getRepos = function(jsonFile){
 
 
             // Do it all again with 2nd 100 repos
-            if(jsonFile == 'repos.json'){
+            if(jsonFile == 'repos1.json'){
                 getRepos('repos2.json')
             }else{
                 // wimProjects.allRepos = allRepos;
@@ -88,7 +88,7 @@ var getRepos = function(jsonFile){
     });
 }
 
-getRepos("repos.json");
+getRepos("repos1.json");
 
 
 
