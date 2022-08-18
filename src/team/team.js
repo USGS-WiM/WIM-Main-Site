4f4ec4c0de027c98b45e2769a176a9b67c909d68
Mitch Samuels <samuels.mitch@gmail.com>
2022-08-18 09:44:34 -0500

    Highlight team when clicked on map

--- src/team/team.js
+++ src/team/team.js
@@ -1,75 +1,184 @@
-// Define WIM Team
-var members = {
-	gary: {
-		name: "Gary Latzke",
-		title: "Program Coordinator",
-		phone: "(608)821-3805",
-		email: "gdlatzke@usgs.gov",
-		bio: "Gary has his degree in Cartography from Northern Illinois University and 27 years of cartographic experience in the USGS. His specialties include cartographic design and communication, digital application design, and implementation of cloud infrastructures for geospatial applications. In addition WIM, Gary serves on the staff of the Information and Technology Management Director for the Department of the Interior, leading and assisting in special projects for the Department. Outside of work Gary enjoys golfing, writing music and making unique cocktails with his wife in their home lounge.", 
-		coordinates: "-89.4012302, 43.0730517"
-	},
-	hans: {
-		name: "Hans Vraga",
-		title: "Project Manager",
-		phone: "(703)648-5670",
-		email: "hvraga@usgs.gov",
-		bio: "Hans got his start as a CH-53 Super Stallion Helicopter Crew Chief from the USMC, callsign: 'Wedgie.' He turned ITIL disciple at the UW-Madison Division of Information Technology (DoIT) while earning a BS in Computer Engineering and Computer Science. After a 8 years at DoIT wearing Configuration, Incident, and Change Management hats, Hans moved to USGS-WIM to head up Project Management duties and earned a PMP certification. Hans currently spends most of his day coordinating a highly skilled and motivated team of developers, doing his best not to slow them down too much. Outside of work, Hans enjoys a good game of ultimate, hiking, camping and the occasional novel.",
-		coordinates: "-93.265011, 44.977753"
-	},
-};
+window.onload=function(){
+
+	console.log("Team Page Loaded 👍")
 
 
-// Team Page Map
-var mapZoomLevel = 4;
-var mapCenter = [37.76, -95.38];
+	// Highlight member with class
+	document.getElementById("testButton").onclick = function() {
+		// Add class to highlight
+		document.getElementById("hans").classList.add("highlight");
 
-// Create Map
-var map = L.map('teamMap', {zoomControl: false, defaultExtentControl: false}).setView(mapCenter, mapZoomLevel);
+		// Remove highlight after 2 seconds
+		setTimeout(function(){
+			document.getElementById("hans").classList.remove("highlight");
+		}, 2000); //wait 2 seconds - 2000 milliseconds
+	};
 
-// // Set Map Options
-var layerLabels;
-map.dragging.disable();
-map.touchZoom.disable();
-map.doubleClickZoom.disable();
-map.scrollWheelZoom.disable();
-map.boxZoom.disable();
-map.keyboard.disable();
-if (map.tap) map.tap.disable();
 
-// Day Theme
-var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 19
-});
-// Night Theme
-var NASAGIBS_ViirsEarthAtNight2012 = L.tileLayer('https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
-	attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
-	bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
-	minZoom: 1,
-	maxZoom: 8,
-	format: 'jpg',
-	time: '',
-	tilematrixset: 'GoogleMapsCompatible_Level'
-});
 
-// Set basemap based on time
-// 7am-8pm, light theme
-// 8pm-7am, Night theme
-var today = new Date()
-var curHr = today.getHours()
-if (curHr < 8) {
-	NASAGIBS_ViirsEarthAtNight2012.addTo(map);
-} else if (curHr < 20) {
-	CartoDB_PositronNoLabels.addTo(map);
-} else {
-	NASAGIBS_ViirsEarthAtNight2012.addTo(map);
-}
 
-// // Center Map
-var centerMap = function(){
-	map.flyTo([43, -97], zoomlvl, {
-		animate: true,
-		duration: 0.7
+	// 
+	// Team Page Map
+	// Team Page Map
+	// Team Page Map
+	// 
+	var mapZoomLevel = 4;
+	var mapCenter = [37.76, -95.38];
+
+	// Create Map
+	var map = L.map('teamMap', {zoomControl: false, defaultExtentControl: false}).setView(mapCenter, mapZoomLevel);
+
+	// // Set Map Options
+	var layerLabels;
+	map.dragging.disable();
+	map.touchZoom.disable();
+	map.doubleClickZoom.disable();
+	map.scrollWheelZoom.disable();
+	map.boxZoom.disable();
+	map.keyboard.disable();
+	if (map.tap) map.tap.disable();
+
+	// Day Theme
+	var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+		subdomains: 'abcd',
+		maxZoom: 19
 	});
-}
+	// Night Theme
+	var NASAGIBS_ViirsEarthAtNight2012 = L.tileLayer('https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
+		attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
+		bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
+		minZoom: 1,
+		maxZoom: 8,
+		format: 'jpg',
+		time: '',
+		tilematrixset: 'GoogleMapsCompatible_Level'
+	});
+
+	// Set basemap based on time
+	// 7am-8pm, light theme
+	// 8pm-7am, Night theme
+	var today = new Date()
+	var curHr = today.getHours()
+	if (curHr < 8) {
+		NASAGIBS_ViirsEarthAtNight2012.addTo(map);
+	} else if (curHr < 20) {
+		CartoDB_PositronNoLabels.addTo(map);
+	} else {
+		NASAGIBS_ViirsEarthAtNight2012.addTo(map);
+	}
+
+	// // Center Map
+	var centerMap = function(){
+		map.flyTo([43, -97], mapZoomLevel, {
+			animate: true,
+			duration: 0.7
+		});
+	}
+
+	// GeoJSON Array of each location
+	// And popup content for map marker popups
+	// with each member at each location
+	var geojsonFeature = [ 
+		{
+			"type": "Feature",
+			"properties": {"popupContent": "<div class='map-popup' onclick='popupClick'><b>Madison, WI</b><button id='Gary'>Gary Latzke</button><button id='Blake'>Blake Draper</button><button id='Erik'>Erik Myers</button><button id='Aaron'>Aaron Stephenson</button><button id='Abby'>Abby Gleason</button><button id='Kip'>Kip Sullivan</button><button id='Lily'>Lily Houtman</button><button id='Milan'>Milan Liu</button></div>" },
+			"geometry": {"type": "Point", "coordinates": [-89.4012302, 43.0730517]},
+		},
+		{
+			"type": "Feature",
+			"properties": {"popupContent": "<div class='map-popup'><b>Twin Cities, MN</b><button id='Hans'>Hans Vraga</button><button id='Mitch'>Mitch Samuels</button><button id='Kathy'>Kathy Dooley</button><button id='Anders'>Anders Hopkins</button><button id='Julia'>Julia Prokopec</button></div>" },
+			"geometry": {"type": "Point", "coordinates": [-93.265011, 44.977753]},
+		},
+		{
+			"type": "Feature",
+			"properties": {"popupContent": "<div class='map-popup'><b>Washington, DC</b><button id='Lauren'>Lauren Privette</button>" },
+			"geometry": {"type": "Point", "coordinates": [-77.036871, 38.907192]},
+		},
+		{
+			"type": "Feature",
+			"properties": {"popupContent": "<div class='map-popup'><b>Davis, CA</b><button id='Nick'>Nick Estes</button>" },
+			"geometry": {"type": "Point", "coordinates": [-121.740517, 38.544907]},
+		},
+		{
+			"type": "Feature",
+			"properties": {"popupContent": "<div class='map-popup'><b>Elgin, IL</b><button id='Lorraine'>Lorraine Metz</button>" },
+			"geometry": {"type": "Point", "coordinates": [-87.88739, 41.83789]},
+		},
+		{
+			"type": "Feature",
+			"properties": {"popupContent": "<div class='map-popup'><b>Bloomington, IN</b><button id='Danny'>Danny Rubin</button>" },
+			"geometry": {"type": "Point", "coordinates": [-86.512627, 39.1754487]},
+		},
+		{
+			"type": "Feature",
+			"properties": {"popupContent": "<div class='map-popup'><b>Raleigh, NC</b><button id='Andrea'>Andrea Medenblik</button></div>" },
+			"geometry": {"type": "Point", "coordinates": [-78.785484, 35.843964]},
+		},
+		{
+			"type": "Feature",
+			"properties": {"popupContent": "<div class='map-popup'><b>Bismarck, ND</b><button id='Harper'>Harper Wavra</button></div>" },
+			"geometry": {"type": "Point", "coordinates": [-100.810489, 46.8115136]},
+		},
+		{
+			"type": "Feature",
+			"properties": {"popupContent": "<div class='map-popup'><b>Orlando, FL</b><button id='Chad'>Chad Fanguy</button></div>" },
+			"geometry": {"type": "Point", "coordinates": [-81.379234, 28.538336]},
+		},
+	];
+
+
+	// WimIcon & popup
+	var myIcon = L.Icon.extend({});
+	var wimIcon = new myIcon({
+		iconUrl: require('../assets/images/map_pin.png'),
+		iconSize: [24.9, 28.95],
+		iconAnchor: [12.45,28.95],
+		popupAnchor: [0,-28.95],
+	});
+
+	// Add points to map
+	var geoJsonLayer = L.geoJSON(geojsonFeature, {
+		pointToLayer: function (feature, latlng) {
+			return L.marker(latlng, {icon: wimIcon}).on('click', function(a) {
+				map.flyTo(latlng, 8, {
+					animate: true,
+					duration: 0.7
+				});
+			}).addTo(map).bindPopup(feature.properties.popupContent);
+
+		}
+	}).addTo(map);
+
+	// Add Markers
+	var markers = L.markerClusterGroup({maxClusterRadius: 1 });
+	markers.addLayer(geoJsonLayer);
+	// On popup close, center map
+	markers.on('popupclose', function(e) {
+		centerMap();
+	});
+
+	// On popup open, listen for clicks
+	markers.on('popupopen', function(e) {
+		document.getElementsByClassName("map-popup")[0].addEventListener('click', highlightMember, false);
+	});
+	// Highlight member when name is clicked
+	var highlightMember = function(memberID) {
+		console.log("Clicked Popup")
+		// If clicked element has an ID, it's a member name
+		if(event.target.id){
+			var memberID = event.target.id.toLowerCase();
+			console.log("Clicked Member")
+			console.log(memberID)
+		}
+	};
+
+	// Add markers to map and set bounds, zoom
+	map.addLayer(markers);
+	map.fitBounds(markers.getBounds());
+	map.setZoom(mapZoomLevel);
+	map.setView([43,-97]);
+
+
+} // End Window Onload
+
